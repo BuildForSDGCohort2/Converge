44b56a306f9b05d740fd794f5d44de6a484213e8
miami78 <miamilarry78@gmail.com>
2020-09-01 21:53:18 +0300

    update user fields

--- src/migration/1598618605109-Refractoring.ts
+++ src/migration/1598618605109-Refractoring.ts
@@ -3,7 +3,7 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class Refractoring1598618605109 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {  
-        await queryRunner.query(`CREATE TABLE "User" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`, undefined);
+        await queryRunner.query(`CREATE TABLE "User" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "email" character varying NOT NULL, "gender" character varying NOT NULL, "country" character varying NOT NULL, "dateOfBirth" date NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`, undefined);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
