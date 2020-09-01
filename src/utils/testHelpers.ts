0727c0069c115e7cb43862a19c64201abeed8c9f
miami78 <miamilarry78@gmail.com>
2020-09-01 21:54:05 +0300

    add update user helper

--- src/utils/testHelpers.ts
+++ src/utils/testHelpers.ts
@@ -2,8 +2,18 @@ export const testUser = {
 firstName: "testFirstName",
 lastName: "testLastName",
 email: "test@test.test",
-password: "test123456",
-role: 1
+gender: "M",
+country: "Kenya",
+dateOfBirth: "29-07-1996",
+password: "test123456"
 };
 
-  
+export const testUserUpdate = {
+firstName: "testFirstNameUpdate",
+lastName: "testLastNameUpdate",
+email: "testUpdate@test.test",
+gender: "F",
+country: "Nigeria",
+dateOfBirth: "20-01-1995",
+password: "testUpdate123",
+};
