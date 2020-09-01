a193ef3bab91d010ce6b828b1271ef6f82b3279b
miami78 <miamilarry78@gmail.com>
2020-09-01 21:01:24 +0300

    add login and get user by id test

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -15,4 +15,38 @@ describe("Users", () => {
       } catch (error) {}
     });
   });
+
+  describe("POST /api/v1/users/login", () => {
+    it("should login registered user", async () => {
+      try {
+        /**create user */
+        let { body } = await request(app)
+          .post(`${BASE_URL}/users/login`)
+          .set("Accept", "application/json")
+          .send(testUser);
+
+        /** Tests */
+        expect(body.status).to.eql(200);
+        expect(body).to.have.property("token");
+        expect(body).to.have.property("message", "OK");
+        expect(body.token).to.be.a("string");
+      } catch (error) {}
+    });
+  });
+
+  describe("GET /api/v1/users/id", () => {
+    it("should retrieve a specific user by id", async () => {
+      try {
+        let { body } = await request(app)
+          .get(`${BASE_URL}/users/1`)
+          .set("Accept", "application/json");
+
+        /** Tests */
+        expect(body.status).to.eql(200);
+        expect(body).to.have.property("firstName");
+        expect(body).to.have.property("lastName");
+        expect(body).to.have.property("email");
+      } catch (error) {}
+    });
+  });
 });
