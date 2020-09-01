2ea37e10a63ff21e98a14f05455456a633523bf2
miami78 <miamilarry78@gmail.com>
2020-09-01 21:00:21 +0300

    add get user by id route

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -1,10 +1,15 @@
-import {register} from "../controllers/userController";
-import {login} from "../controllers/userController";
+import {
+    register,
+    login,
+    getUser,
+} from "../controllers/userController";
+import authMiddleware from "../middleware/authMiddleware";
 import { Router } from "express";
   
 const userRouter = Router();
 userRouter.post("/users/register",register);
 userRouter.post("/users/login",login);
+userRouter.get("/users/:id", authMiddleware, getUser);
 
 export { userRouter };
   
