ec3abc3f8d71c60192115f0e2b4ec21a7340a4a1
Newton <kamaunewton78@gmail.com>
2020-09-07 21:14:08 +0300

    refactored

--- auth/index.js
+++ auth/index.js
@@ -1,21 +1,21 @@
 const express = require('express');
+const app = express(); 
 const mongoose = require('mongoose');
-const app = express();
-
-
-
+//import routes 
+const authRoutes = require('./routes/auth');
 //connection to the mongoose db
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-},()=> console.log("connected to db"));
-//import routes 
-const authRoutes = require('./routes/auth');
-
+}, () => console.log("connected to db"));
 
 //Route middleware
-app.use('./api/user',authRoutes)
+app.use(express.json());
+//api route
+app.use("/api/user", authRoutes);
+
+//port
 app.listen(3000, ()=> console.log('Serving'));
 
 
-//db connection
+
