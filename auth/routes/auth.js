287ed5d9cc9736a362d9b903283ce8d7e35e8445
Newton <kamaunewton78@gmail.com>
2020-09-07 21:15:39 +0300

    use bodyParser to read data and save it to the database

--- auth/routes/auth.js
+++ auth/routes/auth.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+//import User model
+const User = require("../model/User");
 
-router.post('/', (req, res)=>{
-    res.send('register');
+
+
+router.post('/register', async (req, res)=>{
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser)
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 module.exports = router;
