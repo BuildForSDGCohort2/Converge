461d9d97cbb95eef6a4b7f1aa165f18757965e04
Newton <kamaunewton78@gmail.com>
2020-09-09 05:44:22 +0300

    user details validation before saving

--- auth/routes/auth.js
+++ auth/routes/auth.js
@@ -1,21 +1,35 @@
+
 const router = require('express').Router();
 //import User model
 const User = require("../model/User");
+const { body, validationResult } = require('express-validator');
 
-
-
-router.post('/register', async (req, res)=>{
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    try {
-        const savedUser = await user.save();
-        res.send(savedUser)
+router.post("/register", [
+    //name
+    body('name').isLength({ min: 6 }),
+  // username must be an email
+  body('email').isEmail(),
+  // password must be at least 5 chars long
+  body('password').isLength({ min: 6 })
+],async (req, res) => {
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  } 
+        const User = new User({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+          try {
+        const savedUser = await User.save();
+        res.send(savedUser);
     } catch (error) {
         console.log(error);
     }
-})
+});
+
+
 
 module.exports = router;
