a7e635d7b0b6beb76f28226621a79c0925344533
Newton <kamaunewton78@gmail.com>
2020-09-09 05:43:30 +0300

    removed validation from the modee

--- auth/model/User.js
+++ auth/model/User.js
@@ -2,20 +2,13 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    min: 6,
   },
   email: {
     type: String,
-    required: true,
-    max: 255,
-    min: 4,
+
   },
   password: {
     type: String,
-    required: true,
-    max: 1024,
-    min: 4,
     },
     date: {
         type: Date,
