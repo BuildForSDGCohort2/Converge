35386e90f2a054df21c5c7ae63e711ccb37a32be
Newton <kamaunewton78@gmail.com>
2020-09-09 06:53:40 +0300

    check if user exists

--- auth/routes/auth.js
+++ auth/routes/auth.js
@@ -6,9 +6,9 @@ const { body, validationResult } = require('express-validator');
 
 router.post("/register", [
     //name
-    body('name').isLength({ min: 6 }),
+    body('name').isLength({ min: 6 }).notEmpty(),
   // username must be an email
-  body('email').isEmail(),
+  body('email').isEmail().normalizeEmail(),
   // password must be at least 5 chars long
   body('password').isLength({ min: 6 })
 ],async (req, res) => {
@@ -17,16 +17,21 @@ router.post("/register", [
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         } else {
-            const user = new User({
-                name: req.body.username,
-                email: req.body.email,
-                password: req.body.password,
-            });
-            try {
-                const savedUser = await user.save();
-                res.send(savedUser);
-            } catch (error) {
-                console.log(error);
+            //check is user email already exists
+            const emailExist = await User.findOne({ email: req.body.email });
+            if (emailExist) { return res.send("User email exists"); } else {
+                const user = new User({
+                    name: req.body.username,
+                    email: req.body.email,
+                    password: req.body.password,
+                });
+                try {
+                    //save new user 
+                    const savedUser = await user.save();
+                    res.send(savedUser);
+                } catch (error) {
+                    console.log(error);
+                }
             }
         }
 });
