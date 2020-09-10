60b84ef58c3bb047431fc59dc36833a8fd30a824
Newton <kamaunewton78@gmail.com>
2020-09-10 11:25:06 +0300

    login route

--- auth/routes/auth.js
+++ auth/routes/auth.js
@@ -4,7 +4,9 @@ const router = require('express').Router();
 const User = require("../model/User");
 const { body, validationResult } = require('express-validator');
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
+
+
+//register
 router.post("/register", [
     //name
     body('name').isLength({ min: 6 }).notEmpty(),
@@ -39,4 +41,49 @@ router.post("/register", [
             }
         }
 });
+//login
+router.post(
+  "/login",
+  [
+ 
+    // username must be an email
+    body("email").isEmail().normalizeEmail(),
+    // password must be at least 5 chars long
+    body("password").isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+      }
+      else {
+          const salt = await bcrypt.genSalt(10);
+          const hashedPassword = await bcrypt.hash(req.body.password, salt);
+          const user = new User({
+
+              email: req.body.email,
+              password: hashedPassword,
+          });
+     
+          //check is user email already exists
+          const userExists = await User.findOne({ email: req.body.email });
+          if (!userExists) {
+              return res.send("You need to register");
+          }
+          //Check password
+          const validPassword = await bcrypt.compare(
+              req.body.password,
+              user.password
+          );
+          if (!validPassword) {
+              res.send("Wrong password");
+          }
+          else {
+              res.send("Logged in");
+          }
+      }
+  }
+);
+
 module.exports = router;
